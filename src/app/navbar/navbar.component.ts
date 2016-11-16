df5cbcc92c253791272933039c8cb7bbd3e8118f
lichenbuliren <617106268@qq.com>
2016-11-16 15:45:47 +0800

    新增头部导航栏

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from "@angular/platform-browser";
+import { Router, ActivatedRoute, NavigationEnd } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
@@ -6,8 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  private _title: string;
 
-  constructor() { }
+  constructor(
+    private titleService: Title,
+    private router: Router) {
+
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this._title = this.getTitle(router.routerState, router.routerState.root).join(' - ');
+        console.log(this._title);
+        titleService.setTitle(this._title);
+      }
+    });
+  }
+
+  getTitle(state, parent) {
+    var data = [];
+    if (parent && parent.snapshot.data && parent.snapshot.data.title) {
+      data.push(parent.snapshot.data.title);
+    }
+
+    if (state && parent) {
+      data.push(...this.getTitle(state, state.firstChild(parent)));
+    }
+
+    return data;
+  }
+
 
   ngOnInit() {
   }
