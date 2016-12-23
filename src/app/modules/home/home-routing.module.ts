0b0ac3dcbc42bdfc76d31317dd490038fe03765d
lichenbuliren <617106268@qq.com>
2016-12-23 16:55:25 +0800

    update

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -13,8 +13,8 @@ import { HomeSubmitComponent } from "./home-submit/home-submit.component";
         component: HomeComponent,
         data: { title: '上门快修' },
         children: [
-          { path: 'verify', component: HomeVerifyComponent },
-          { path: 'submit', component: HomeSubmitComponent },
+          { path: 'verify', component: HomeVerifyComponent, data: { title: '基础信息'} },
+          { path: 'submit', component: HomeSubmitComponent, data: { title: '维修信息'} },
           { path: '', redirectTo: 'verify', pathMatch: 'full' },
           { path: '**', redirectTo: 'verify' }
         ]
